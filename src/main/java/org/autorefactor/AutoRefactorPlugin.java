b814b6f2912d177b61fc2289fd76c1a28501614f
JnRouvignac <jn.rouvignac@gmail.com>
2013-05-22 10:28:14 +0200

    Renamed Activator.java to AutoRefactorPlugin.java

--- src/main/java/org/autorefactor/AutoRefactorPlugin.java
+++ src/main/java/org/autorefactor/AutoRefactorPlugin.java
@@ -32,13 +32,13 @@ import org.osgi.framework.BundleContext;
 /**
  * The activator class controls the plug-in life cycle.
  */
-public class Activator extends AbstractUIPlugin {
+public class AutoRefactorPlugin extends AbstractUIPlugin {
 
 	/** The plug-in ID */
 	public static final String PLUGIN_ID = "AutoRefactor";
 
 	/** The shared instance */
-	private static Activator plugin;
+	private static AutoRefactorPlugin plugin;
 
 	@Override
 	public void start(final BundleContext context) throws Exception {
@@ -54,17 +54,17 @@ public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 * 
+	 *
 	 * @return the shared instance
 	 */
-	public static Activator getDefault() {
+	public static AutoRefactorPlugin getDefault() {
 		return plugin;
 	}
 
 	/**
 	 * Returns an image descriptor for the image file at the given plug-in
 	 * relative path
-	 * 
+	 *
 	 * @param path
 	 *            the path
 	 * @return the image descriptor
