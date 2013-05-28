ec68e482aabc52fafd708e1079cf1cc2ef1fb5c9
Jean-Noël Rouvignac <jn.rouvignac@gmail.com>
2013-05-28 08:35:55 +0200

    Added test samples for CFGBuilder

---
+++ test/org/autorefactor/cfg/LabelsSample.java
@@ -0,0 +1,60 @@
+/*
+ * AutoRefactor - Eclipse plugin to automatically refactor Java code bases.
+ *
+ * Copyright (C) 2013 Jean-Noël Rouvignac - initial API and implementation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program under LICENSE-GNUGPL.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution under LICENSE-ECLIPSE, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.autorefactor.cfg;
+
+public class LabelsSample {
+
+	public void sample(String[] strings, boolean b1, boolean b2, boolean b3) {
+		outerLoop: for (String s : strings) {
+			for (String s : strings) {
+				if (b1 && b2 && b3) {
+					break outerLoop;
+				}
+				if (b1 && b2) {
+					continue outerLoop;
+				}
+				if (b1) {
+					break;
+				}
+				if (b2) {
+					continue;
+				}
+				if (b3) {
+					return;
+				}
+			}
+		}
+
+		disguisedGoto: {
+			int i = 0;
+			break disguisedGoto;
+			i++;
+			return;
+			i++;
+		}
+	}
+
+}
