b01988695d0b84d83d759a16cf1872597d548ef5
Jean-Noël Rouvignac <jn.rouvignac@gmail.com>
2015-02-17 10:30:38 +0100

    Updated copyrights for files updated in 2015

--- plugin/src/main/java/org/autorefactor/refactoring/rules/HotSpotIntrinsicedAPIsRefactoring.java
+++ plugin/src/main/java/org/autorefactor/refactoring/rules/HotSpotIntrinsicedAPIsRefactoring.java
@@ -1,7 +1,7 @@
 /*
  * AutoRefactor - Eclipse plugin to automatically refactor Java code bases.
  *
- * Copyright (C) 2014 Jean-Noël Rouvignac - initial API and implementation
+ * Copyright (C) 2014-2015 Jean-Noël Rouvignac - initial API and implementation
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -47,7 +47,6 @@ import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 
 import static org.autorefactor.refactoring.ASTHelper.*;
 import static org.autorefactor.util.Utils.*;
-
 /**
  * Refactors code patterns to use intrinsiced APIs in Hotspot JVM.
  * intrinsics are APIs that receive special treatment when JITed:
