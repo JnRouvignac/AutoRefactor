2d9de1aff5b5b3501f9ac222ee08a2cc4ed3e69f
Jean-Noël Rouvignac <jn.rouvignac@gmail.com>
2016-08-06 14:00:00 +0200

    Update copyright years from latest commits

--- plugin/src/main/java/org/autorefactor/ui/preferences/EclipsePreferences.java
+++ plugin/src/main/java/org/autorefactor/ui/preferences/EclipsePreferences.java
@@ -1,7 +1,7 @@
 /*
  * AutoRefactor - Eclipse plugin to automatically refactor Java code bases.
  *
- * Copyright (C) 2013-2014 Jean-Noël Rouvignac - initial API and implementation
+ * Copyright (C) 2013-2016 Jean-Noël Rouvignac - initial API and implementation
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
