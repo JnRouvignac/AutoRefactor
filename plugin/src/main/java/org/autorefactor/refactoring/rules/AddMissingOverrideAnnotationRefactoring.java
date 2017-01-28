282da3626a52b508f6be8eae831f7f2b6228531c
Arnaud Nauwynck <arnaud.nauwynck@gmail.com>
2017-01-28 12:10:40 +0100

    fix checkstyle

--- plugin/src/main/java/org/autorefactor/refactoring/rules/AddMissingOverrideAnnotationRefactoring.java
+++ plugin/src/main/java/org/autorefactor/refactoring/rules/AddMissingOverrideAnnotationRefactoring.java
@@ -38,76 +38,76 @@ import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.internal.corext.util.MethodOverrideTester;
 import org.eclipse.jdt.internal.corext.util.SuperTypeHierarchyCache;
 
-/** 
- * Automated refactoring to "Add missing @Override annotations" to methods 
+/**
+ * Automated refactoring to "Add missing @Override annotations" to methods.
  */
 @SuppressWarnings("restriction")
 public class AddMissingOverrideAnnotationRefactoring extends AbstractRefactoringRule {
 
-	private static final String OVERRIDE = "Override";
+    private static final String OVERRIDE = "Override";
 
-	@Override
-	public String getDescription() {
-		return "Add missing @Override annotation";
-	}
+    @Override
+    public String getDescription() {
+        return "Add missing @Override annotation";
+    }
 
-	@Override
-	public String getName() {
-		return "Add missing @Override annotation";
-	}
-	
-	@Override
-	public boolean visit(MethodDeclaration node) {
-		@SuppressWarnings("unchecked")
-		List<IExtendedModifier> modifiers = node.modifiers();
-		if (-1 != findOverridenAnnotationModifier(modifiers)) {
-			return true;
-		}
-		IMethod overridenMeth = findOverridenMethod(node);
-		if (overridenMeth != null) {
-			// detected missing @Overriden => add it
-			final ASTBuilder b = ctx.getASTBuilder();
-			Annotation overrideAnnotation = b.annotation(OVERRIDE);
-			ctx.getRefactorings().insertAt(node, MethodDeclaration.MODIFIERS2_PROPERTY, overrideAnnotation, 0);
-		}
-		return true;
-	}
-	
-	/** @return overriden method, or null if not found/failed */
-	private static IMethod findOverridenMethod(MethodDeclaration meth) {
-		IMethod res = null;
-		IMethod imeth = bindingToIMethod(meth);
-		if (imeth != null) {
-			IType itype = imeth.getDeclaringType();
-			try {
-				MethodOverrideTester methodOverrideTester = SuperTypeHierarchyCache.getMethodOverrideTester(itype);
-				res = methodOverrideTester.findOverriddenMethod(imeth, true);
-			} catch(Exception ex) {
-				res = null; // ignore, no rethrow!
-			}
-		}
-		return res;
-	}
-	
-	/** @return index if exist of "@Override" MarkerAnnotation element in list */
-	private static int findOverridenAnnotationModifier(List<IExtendedModifier> modifiers) {
-		for (int i = 0; i < modifiers.size(); i++) {
-			IExtendedModifier m = modifiers.get(i);
-			if (m instanceof MarkerAnnotation) {
-				MarkerAnnotation ma = (MarkerAnnotation) m;
-				String fqn = ma.getTypeName().getFullyQualifiedName();
-				if (fqn.equals(OVERRIDE)) {
-					return i;
-				}
-			}
-		}
-		return -1;
-	}
-	
-	/** @return corresponding IMethod of AST method declaration */
-	private static IMethod bindingToIMethod(MethodDeclaration node) {
-		IMethodBinding binding = node.resolveBinding();
-		return (binding != null)? (IMethod) binding.getJavaElement() : null;
-	}
-	
+    @Override
+    public String getName() {
+        return "Add missing @Override annotation";
+    }
+
+    @Override
+    public boolean visit(MethodDeclaration node) {
+        @SuppressWarnings("unchecked")
+        List<IExtendedModifier> modifiers = node.modifiers();
+        if (-1 != findOverridenAnnotationModifier(modifiers)) {
+            return true;
+        }
+        IMethod overridenMeth = findOverridenMethod(node);
+        if (overridenMeth != null) {
+            // detected missing @Overriden => add it
+            final ASTBuilder b = ctx.getASTBuilder();
+            Annotation overrideAnnotation = b.annotation(OVERRIDE);
+            ctx.getRefactorings().insertAt(node, MethodDeclaration.MODIFIERS2_PROPERTY, overrideAnnotation, 0);
+        }
+        return true;
+    }
+
+    /** @return overriden method, or null if not found/failed. */
+    private static IMethod findOverridenMethod(MethodDeclaration meth) {
+        IMethod res = null;
+        IMethod imeth = bindingToIMethod(meth);
+        if (imeth != null) {
+            IType itype = imeth.getDeclaringType();
+            try {
+                MethodOverrideTester methodOverrideTester = SuperTypeHierarchyCache.getMethodOverrideTester(itype);
+                res = methodOverrideTester.findOverriddenMethod(imeth, true);
+            } catch (Exception ex) {
+                res = null; // ignore, no rethrow!
+            }
+        }
+        return res;
+    }
+
+    /** @return index if exist of "@Override" MarkerAnnotation element in list. */
+    private static int findOverridenAnnotationModifier(List<IExtendedModifier> modifiers) {
+        for (int i = 0; i < modifiers.size(); i++) {
+            IExtendedModifier m = modifiers.get(i);
+            if (m instanceof MarkerAnnotation) {
+                MarkerAnnotation ma = (MarkerAnnotation) m;
+                String fqn = ma.getTypeName().getFullyQualifiedName();
+                if (fqn.equals(OVERRIDE)) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /** @return corresponding IMethod of AST method declaration. */
+    private static IMethod bindingToIMethod(MethodDeclaration node) {
+        IMethodBinding binding = node.resolveBinding();
+        return (binding != null) ? (IMethod) binding.getJavaElement() : null;
+    }
+
 }
