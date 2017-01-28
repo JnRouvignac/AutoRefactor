b4af1f91b6901c1729631dfdded45110b450852e
Arnaud Nauwynck <arnaud.nauwynck@gmail.com>
2017-01-28 12:00:47 +0100

    added license + added javadoc + beautify code

--- plugin/src/main/java/org/autorefactor/refactoring/rules/AddMissingOverrideAnnotationRefactoring.java
+++ plugin/src/main/java/org/autorefactor/refactoring/rules/AddMissingOverrideAnnotationRefactoring.java
@@ -1,3 +1,28 @@
+/*
+ * AutoRefactor - Eclipse plugin to automatically refactor Java code bases.
+ *
+ * Copyright (C) 2016 Jean-Noël Rouvignac - initial API and implementation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program under LICENSE-GNUGPL.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution under LICENSE-ECLIPSE, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.autorefactor.refactoring.rules;
 
 import java.util.List;
@@ -13,6 +38,9 @@ import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.internal.corext.util.MethodOverrideTester;
 import org.eclipse.jdt.internal.corext.util.SuperTypeHierarchyCache;
 
+/** 
+ * Automated refactoring to "Add missing @Override annotations" to methods 
+ */
 @SuppressWarnings("restriction")
 public class AddMissingOverrideAnnotationRefactoring extends AbstractRefactoringRule {
 
@@ -27,39 +55,42 @@ public class AddMissingOverrideAnnotationRefactoring extends AbstractRefactoring
 	public String getName() {
 		return "Add missing @Override annotation";
 	}
-
-//	@Override
-//    public boolean isEnabled(final Preferences prefs) {
-//        return false; // prefs.addMissingOverrideAnnotations();
-//    }
 	
+	@Override
 	public boolean visit(MethodDeclaration node) {
-		try {
-			@SuppressWarnings("unchecked")
-			List<IExtendedModifier> modifiers = node.modifiers();
-			if (-1 != findOverridenAnnotationModifier(modifiers)) {
-				return true;
-			}
-			IMethod imeth = bindingToIMethod(node);
-			if (imeth != null) {
-				IType itype = imeth.getDeclaringType();
-				MethodOverrideTester methodOverrideTester = SuperTypeHierarchyCache.getMethodOverrideTester(itype);
-				IMethod overridenMeth = methodOverrideTester.findOverriddenMethod(imeth, true);
-				if (overridenMeth != null) {
-					// detected missing @Overriden
-					final ASTBuilder b = ctx.getASTBuilder();
-					Annotation overrideAnnotation = b.annotation(OVERRIDE);
-					// modifiers.add(0, overrideAnnotation);
-					ctx.getRefactorings().insertAt(node, MethodDeclaration.MODIFIERS2_PROPERTY, overrideAnnotation, 0);
-				}
-			}
-		} catch(Exception ex) {
-			// ignore!
+		@SuppressWarnings("unchecked")
+		List<IExtendedModifier> modifiers = node.modifiers();
+		if (-1 != findOverridenAnnotationModifier(modifiers)) {
+			return true;
+		}
+		IMethod overridenMeth = findOverridenMethod(node);
+		if (overridenMeth != null) {
+			// detected missing @Overriden => add it
+			final ASTBuilder b = ctx.getASTBuilder();
+			Annotation overrideAnnotation = b.annotation(OVERRIDE);
+			ctx.getRefactorings().insertAt(node, MethodDeclaration.MODIFIERS2_PROPERTY, overrideAnnotation, 0);
 		}
 		return true;
 	}
 	
-	public static int findOverridenAnnotationModifier(List<IExtendedModifier> modifiers) {
+	/** @return overriden method, or null if not found/failed */
+	private static IMethod findOverridenMethod(MethodDeclaration meth) {
+		IMethod res = null;
+		IMethod imeth = bindingToIMethod(meth);
+		if (imeth != null) {
+			IType itype = imeth.getDeclaringType();
+			try {
+				MethodOverrideTester methodOverrideTester = SuperTypeHierarchyCache.getMethodOverrideTester(itype);
+				res = methodOverrideTester.findOverriddenMethod(imeth, true);
+			} catch(Exception ex) {
+				res = null; // ignore, no rethrow!
+			}
+		}
+		return res;
+	}
+	
+	/** @return index if exist of "@Override" MarkerAnnotation element in list */
+	private static int findOverridenAnnotationModifier(List<IExtendedModifier> modifiers) {
 		for (int i = 0; i < modifiers.size(); i++) {
 			IExtendedModifier m = modifiers.get(i);
 			if (m instanceof MarkerAnnotation) {
@@ -73,11 +104,10 @@ public class AddMissingOverrideAnnotationRefactoring extends AbstractRefactoring
 		return -1;
 	}
 	
-	public static IMethod bindingToIMethod(MethodDeclaration node) {
+	/** @return corresponding IMethod of AST method declaration */
+	private static IMethod bindingToIMethod(MethodDeclaration node) {
 		IMethodBinding binding = node.resolveBinding();
-		if (binding != null) // Bindings can be null
-			return (IMethod) binding.getJavaElement();
-		return null;
+		return (binding != null)? (IMethod) binding.getJavaElement() : null;
 	}
 	
 }
