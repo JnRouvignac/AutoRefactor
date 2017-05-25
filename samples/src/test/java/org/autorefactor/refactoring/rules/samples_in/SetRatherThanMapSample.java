2c3400e63403c20843044852681e0391d7be31ef
Fabrice TIERCELIN <fabrice.tiercelin@gmail.com>
2017-05-25 08:07:41 +0200

    New refactoring: Replace HashMap by HashSet when values are not read

---
+++ samples/src/test/java/org/autorefactor/refactoring/rules/samples_in/SetRatherThanMapSample.java
@@ -0,0 +1,199 @@
+/*
+ * AutoRefactor - Eclipse plugin to automatically refactor Java code bases.
+ *
+ * Copyright (C) 2017 Fabrice Tiercelin - initial API and implementation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program under LICENSE-GNUGPL.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution under LICENSE-ECLIPSE, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.autorefactor.refactoring.rules.samples_in;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class SetRatherThanMapSample {
+
+    public void replaceHashMapInstanceCreation() {
+        // Keep this comment
+        boolean isInside = new HashMap<String, String>().isEmpty();
+        // Keep this comment too
+        int size = new HashMap<String, String>(10).size();
+    }
+
+    public void replaceRawHashMap() {
+        // Keep this comment
+        boolean isInside = new HashMap().isEmpty();
+        // Keep this comment too
+        int size = new HashMap(10).size();
+    }
+
+    public void replaceFullyQualifiedHashMap() {
+        // Keep this comment
+        boolean isInside = new java.util.HashMap<Integer, Date>().isEmpty();
+        // Keep this comment too
+        int size = new java.util.HashMap(10).size();
+    }
+
+    public void replaceHashMapVariableDeclaration() {
+        // Keep this comment
+        HashMap<String, String> aggregate = new HashMap<String, String>();
+    }
+
+    public void doNotReplaceInterface() {
+        // Keep this comment
+        Map<String, String> map = new HashMap<String, String>();
+    }
+
+    public void replaceHashMapVariableUse() {
+        // Keep this comment
+        HashMap<String, String> aggregate = new HashMap<String, String>();
+        // Keep this comment too
+        aggregate.put("foo", "bar");
+    }
+
+    public void refactorMethod() {
+        // Keep this comment
+        HashMap<String, String> aggregate = new HashMap<String, String>();
+        // Keep this comment too
+        aggregate.containsKey("foo");
+    }
+
+    public int replaceHashMapWithLoop(List<Date> dates) {
+        // Keep this comment
+        HashMap<Long, Date> aggregate = new HashMap<Long, Date>();
+        for (Date date : dates) {
+            aggregate.put(date.getTime(), date);
+        }
+
+        return aggregate.size();
+    }
+
+    public int doNotRemoveActiveCode(List<Date> referenceDates, List<Date> dates) {
+        HashMap<Long, Boolean> aggregate = new HashMap<Long, Boolean>();
+        for (Date date : referenceDates) {
+            aggregate.put(date.getTime(), dates.remove(date));
+        }
+
+        return aggregate.size();
+    }
+
+    public void replaceHashMapWithModifier() {
+        // Keep this comment
+        final HashMap<String, String> aggregate = new HashMap<String, String>();
+        aggregate.put("foo", "bar");
+    }
+
+    public void replaceHashMapWithParameter() {
+        // Keep this comment
+        HashMap<String, String> aggregate = new HashMap<String, String>(10);
+        aggregate.put("foo", "bar");
+    }
+
+    public int replaceReassignedHashMap() {
+        // Keep this comment
+        HashMap<String, String> aggregate1 = new HashMap<String, String>();
+        aggregate1.put("foo", "FOO");
+
+        // Keep this comment too
+        HashMap<String, String> aggregate2 = aggregate1;
+        aggregate2.put("bar", "BAR");
+
+        return aggregate2.size();
+    }
+
+    public void doNotReplaceHashMapParameter(HashMap<String, String> aMap) {
+        HashMap<String, String> map = aMap;
+        map.put("foo", "bar");
+    }
+
+    public void doNotReplaceHashMapPassedToAMethod() {
+        String text = String.valueOf(new HashMap<String, String>());
+    }
+
+    public HashMap<Integer, Date> doNotReplaceReturnedHashMap() {
+        return new HashMap<Integer, Date>();
+    }
+
+    public void doNotReplaceReassignedVariable() {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map = new HashMap<String, String>();
+    }
+
+    public void replaceHashMapInAnnomymousClass() {
+        final HashMap<String, String> aggregate = new HashMap<String, String>();
+        new Runnable() {
+
+            @Override
+            public void run() {
+                aggregate.put("foo", "bar");
+            }
+        };
+    }
+
+    public void doNotReplaceSpecificInstantiation(HashMap<String, String> aMap) {
+        HashMap<String, String> map = new HashMap<String, String>(aMap);
+        map.put("foo", "bar");
+    }
+
+    public Object doNotReplaceCloneMethod() {
+        HashMap<String, String> map = new HashMap<String, String>();
+        return map.clone();
+    }
+
+    public boolean doNotReplaceContainsValueMethod() {
+        HashMap<String, String> map = new HashMap<String, String>();
+        return map.containsValue("bar");
+    }
+
+    public Collection<String> doNotReplaceValuesMethod() {
+        HashMap<String, String> map = new HashMap<String, String>();
+        return map.values();
+    }
+
+    public Set<Entry<String, String>> doNotReplaceEntrySetMethod() {
+        HashMap<String, String> map = new HashMap<String, String>();
+        return map.entrySet();
+    }
+
+    public boolean doNotReplaceEqualsMethod() {
+        HashMap<String, String> map = new HashMap<String, String>();
+        return map.equals(123);
+    }
+
+    public String doNotReplaceGetMethod() {
+        HashMap<String, String> map = new HashMap<String, String>();
+        return map.get("foo");
+    }
+
+    public int doNotReplaceHashCodeMethod() {
+        HashMap<String, String> map = new HashMap<String, String>();
+        return map.hashCode();
+    }
+
+    public String doNotReplaceToStringMethod() {
+        HashMap<String, String> map = new HashMap<String, String>();
+        return map.toString();
+    }
+}
