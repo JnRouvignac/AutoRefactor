097a2efddee880f6aa6e110622212df54e0b3a6c
Fabrice TIERCELIN <fabrice.tiercelin@gmail.com>
2017-06-09 22:05:40 +0200

    Fix a TODO

--- plugin.tests/src/test/java/org/autorefactor/test/TestHelper.java
+++ plugin.tests/src/test/java/org/autorefactor/test/TestHelper.java
@@ -33,6 +33,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -78,24 +79,7 @@ public final class TestHelper {
     }
 
     public static String readAll(File file) throws IOException {
-        // FIXME Java 7 version of this method:
-        // return new String(Files.readAllBytes(file.toPath()), "UTF8");
-        FileInputStream fis = null;
-        try {
-            fis = new FileInputStream(file);
-            final InputStreamReader reader = new InputStreamReader(fis);
-            final StringBuilder sb = new StringBuilder();
-            final char[] buf = new char[4096];
-            int nbRead;
-            while ((nbRead = reader.read(buf)) != -1) {
-                sb.append(buf, 0, nbRead);
-            }
-            return sb.toString();
-        } finally {
-            if (fis != null) {
-                fis.close();
-            }
-        }
+        return new String(Files.readAllBytes(file.toPath()), "UTF8");
     }
 
     public static JavaProjectOptions newJavaProjectOptions(Release javaSE, int tabSize) {
