d1bd8d2fc55a5a9e700008fecfd74e36ab1cc269
Fabrice TIERCELIN <fabrice.tiercelin@gmail.com>
2019-11-16 11:28:06 +0100

    Fix CS

--- plugin/src/main/java/org/autorefactor/jdt/internal/corext/dom/AbstractRefactoringJob.java
+++ plugin/src/main/java/org/autorefactor/jdt/internal/corext/dom/AbstractRefactoringJob.java
@@ -1,22 +1,50 @@
+/*
+ * AutoRefactor - Eclipse plugin to automatically refactor Java code bases.
+ *
+ * Copyright (C) 2019 Michael Keppler - initial API and implementation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program under LICENSE-GNUGPL.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution under LICENSE-ECLIPSE, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.autorefactor.jdt.internal.corext.dom;
 
 import org.eclipse.core.runtime.jobs.Job;
 
 /**
  * Abstract super class of all refactoring jobs, marking them as belonging to a certain family, thereby simplifying the cancellation of multiple jobs.
- *
  */
 public abstract class AbstractRefactoringJob extends Job {
-
+    /**
+     * Abstract super class of all refactoring jobs.
+     *
+     * @param name The job name
+     */
     public AbstractRefactoringJob(String name) {
         super(name);
     }
 
     /**
-     * all instances of this job belong to this family
+     * All instances of this job belong to this family.
      */
     public static final Object FAMILY = new Object();
-    
+
     @Override
     public final boolean belongsTo(Object family) {
         if (FAMILY == family) {
@@ -24,5 +52,4 @@ public abstract class AbstractRefactoringJob extends Job {
         }
         return super.belongsTo(family);
     }
-
 }
